164f56a1a8835f511a176ec9b81d8a3a076c5bdf
Kanilive1995 <ladavemoore@gmail.com>
2026-01-10 21:56:09 -0500

    Package config
    made small changes in order for gh pages to correctly host the site

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Portfolio/' // MUST match your GitHub repo name
-});
+  base: '/portfolio/'
+})
